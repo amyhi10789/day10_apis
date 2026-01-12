c35675dbef0f31c45bda6053fdece5c7ffb9396b
amyhi10789 <awang28@eastsideprep.org>
2026-01-12 13:55:53 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+let dataStore = [{ name: "Teacher", message: "API is live!" }];
+
+app.get('/api/messages', (req, res) => {
+    res.json(dataStore);
+});
+
+app.post('/api/messages', (req, res) => {
+    dataStore.push(req.body);
+    res.status(201).send({ message: "Received!" });
+});
+
+app.listen(PORT, () => console.log(`Server: https://localhost:${PORT}`));
